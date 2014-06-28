f99528cf0272e5a94f8dff3cb67db86311747acf
ruthwickp <ruthwickp@yahoo.com>
2014-06-28 12:35:06 -0700

    Added card images

--- MatchKing/PlayingCardView.m
+++ MatchKing/PlayingCardView.m
@@ -116,13 +116,14 @@
     
 }
 
-#define FACE_CARD_SCALE_FACTOR .9
+#define FACE_CARD_SCALE_FACTOR .80
 /**
  * Draws face image on card if found.
  *
  */
 - (void)drawFace
 {
+    // If face image is available
     UIImage *faceImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@%@", [self rankAsString], self.suit]];
     if (faceImage) {
         CGRect imageRect = CGRectInset(self.bounds,
@@ -130,7 +131,104 @@
                                        self.bounds.size.height * (1.0 - FACE_CARD_SCALE_FACTOR));
         [faceImage drawInRect:imageRect];
     }
+    else {
+       [self drawPips];
+    }
 
+}
+
+#define PIP_FONT_SCALE_FACTOR 0.20
+#define PIP_HOFFSET_PERCENTAGE 0.165
+#define PIP_VOFFSET1_PERCENTAGE 0.090
+#define PIP_VOFFSET2_PERCENTAGE 0.175
+#define PIP_VOFFSET3_PERCENTAGE 0.270
+
+/**
+ * Draws pips on view.
+ *
+ */
+- (void)drawPips
+{
+    if ((self.rank == 1) || (self.rank == 5) || (self.rank == 9) || (self.rank == 3)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 6) || (self.rank == 7) || (self.rank == 8)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 2) || (self.rank == 3) || (self.rank == 7) || (self.rank == 8) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:PIP_VOFFSET2_PERCENTAGE
+                        mirroredVertically:(self.rank != 7)];
+    }
+    if ((self.rank == 4) || (self.rank == 5) || (self.rank == 6) || (self.rank == 7) || (self.rank == 8) || (self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET3_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+    if ((self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET1_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+}
+
+/**
+ * Draw pips in certain locations.
+ *
+ */
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                          upsideDown:(BOOL)upsideDown
+{
+    if (upsideDown) [self pushContextAndRotateUpsideDown];
+    CGPoint middle = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+    UIFont *pipFont = [UIFont systemFontOfSize:self.bounds.size.width * PIP_FONT_SCALE_FACTOR];
+    NSAttributedString *attributedSuit = [[NSAttributedString alloc] initWithString:self.suit attributes:@{ NSFontAttributeName : pipFont }];
+    CGSize pipSize = [attributedSuit size];
+    CGPoint pipOrigin = CGPointMake(
+                                    middle.x-pipSize.width/2.0-hoffset*self.bounds.size.width,
+                                    middle.y-pipSize.height/2.0-voffset*self.bounds.size.height
+                                    );
+    [attributedSuit drawAtPoint:pipOrigin];
+    if (hoffset) {
+        pipOrigin.x += hoffset*2.0*self.bounds.size.width;
+        [attributedSuit drawAtPoint:pipOrigin];
+    }
+    if (upsideDown) [self popContext];
+}
+
+// Draw pips in mirrored state
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                  mirroredVertically:(BOOL)mirroredVertically
+{
+    [self drawPipsWithHorizontalOffset:hoffset
+                        verticalOffset:voffset
+                            upsideDown:NO];
+    if (mirroredVertically) {
+        [self drawPipsWithHorizontalOffset:hoffset
+                            verticalOffset:voffset
+                                upsideDown:YES];
+    }
+}
+
+// Rotates current context
+- (void)pushContextAndRotateUpsideDown
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
+    CGContextRotateCTM(context, M_PI);
+}
+
+// Gets current context
+- (void)popContext
+{
+    CGContextRestoreGState(UIGraphicsGetCurrentContext());
 }
 
 
@@ -149,20 +247,18 @@
 - (void)awakeFromNib
 {
     [self setup];
-    self.rank = 13;
+    self.rank = 10;
     self.suit = @"♥️";
     self.faceUp = YES;
     
 }
 
-
-
-
+// Sets the frame
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        [self setup];
     }
     return self;
 }
